b7185aaafac0c36f7dc384f642d1bf7c557bc550
jcanadas <joecanadas@gmail.com>
2023-07-12 21:45:20 -0400

    chore: movie tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
+import  MovieCard  from "./MovieCard";
+import SearchIcon from './search.svg';
 import './App.css';
 
-function App() {
+
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=1e3f3754";
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(() =>{
+        searchMovies("Jaws");
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Joe Test too
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Movie Land</h1>
+
+        <div className="search">
+            <input
+                placeholder="Search for a movie"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <img
+                src={SearchIcon}
+                alt="search"
+                onClick={() => searchMovies(searchTerm)}
+            />
+        </div>
+
+        {
+            movies?.length > 0
+            ?(
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>
+            ) : (
+                <div className={"empty"}>
+                    <h2>No Movies Found</h2>
+                </div>
+            )
+        }
+
     </div>
   );
 }
---
+++ src/Person.js
@@ -0,0 +1,10 @@
+const Person = (props) => {
+    return (
+        <>
+            <h1>Name: {props.name}</h1>
+            <h1>Email: joecanadas@gmail.com</h1>
+        </>
+
+    )
+}
+export default Person;
